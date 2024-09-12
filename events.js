9f149182b9b6ac05a5df7905b5b1bd3d77488fe8
MaikReyesHispam <michael.reyes@telefonica.com>
2024-09-12 16:55:55 -0500

    event and photos

--- events.js
+++ events.js
@@ -1,16 +1,111 @@
 const eventsCalendar = [
     {
         id: 1,
-        name: "",
-        date: "",
-        image: "",
-        distance: "",
-        location: "",
-        elevationGain: "",
-        category: "",
-        organizationName: "",
-        organizationLogo: "",
+        name: "Expedición - Bodytech - Bogotá",
+        date: "2024-09-22T8:00:00Z",
+        image: "images-events/2024/septiembre/bodytech/bodytech-kv.webp",
+        distance: "12k",
+        place: "Bogotá - Plaza de Alfiles",
+        elevationGain: 0,
+        category: "Running",
+        organizationName: "Bodytech",
+        organizationLogo: "images-events/2024/septiembre/bodytech/bodytech.jpg",
         price: "",
+        urlSite: "https://eventos.bodytech.co/event/info/expedicion-bodytech-bogota",
+        location: {
+            latitude: 4.648825372481053,
+            length: -74.10133728464123
+        }
+    },
+    {
+        id: 2,
+        name: "Expedición - Bodytech - Bogotá",
+        date: "2024-09-22T8:00:00Z",
+        image: "images-events/2024/septiembre/bodytech/bodytech-kv.webp",
+        distance: "6k",
+        place: "Bogotá - Plaza de Alfiles",
+        elevationGain: 0,
+        category: "Running",
+        organizationName: "Bodytech",
+        organizationLogo: "images-events/2024/septiembre/bodytech/bodytech.jpg",
+        price: "",
+        urlSite: "https://eventos.bodytech.co/event/info/expedicion-bodytech-bogota",
+        location: {
+            latitude: 4.648825372481053,
+            length: -74.10133728464123
+        }
+    },
+    {
+        id: 3,
+        name: "Expedición - Bodytech - Bogotá",
+        date: "2024-09-22T8:00:00Z",
+        image: "images-events/2024/septiembre/bodytech/bodytech-kv.webp",
+        distance: "3k",
+        place: "Bogotá - Plaza de Alfiles",
+        elevationGain: 0,
+        category: "Running",
+        organizationName: "Bodytech",
+        organizationLogo: "images-events/2024/septiembre/bodytech/bodytech.jpg",
+        price: "",
+        urlSite: "https://eventos.bodytech.co/event/info/expedicion-bodytech-bogota",
+        location: {
+            latitude: 4.648825372481053,
+            length: -74.10133728464123
+        }
+    },
+    {
+        id: 4,
+        name: "Corre con el Alma",
+        date: "2024-09-22T8:00:00Z",
+        image: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma-kv.jpeg",
+        distance: "5k",
+        place: "Manizales",
+        elevationGain: 0,
+        category: "Running",
+        organizationName: "Soul 23 sports",
+        organizationLogo: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma.jpg",
+        price: "",
+        urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
+        location: {
+            latitude: 4.648825372481053,
+            length: -74.10133728464123
+        }
+    },
+    {
+        id: 5,
+        name: "Corre con el Alma",
+        date: "2024-09-22T8:00:00Z",
+        image: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma-kv.jpeg",
+        distance: "10k",
+        place: "Manizales",
+        elevationGain: 0,
+        category: "Running",
+        organizationName: "Soul 23 sports",
+        organizationLogo: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma.jpg",
+        price: "",
+        urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
+        location: {
+            latitude: 4.648825372481053,
+            length: -74.10133728464123
+        }
+    },
+    {
+        id: 5,
+        name: "Corre con el Alma",
+        date: "2024-09-22T8:00:00Z",
+        image: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma-kv.jpeg",
+        distance: "15k",
+        place: "Manizales",
+        elevationGain: 0,
+        category: "Running",
+        organizationName: "Soul 23 sports",
+        organizationLogo: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma.jpg",
+        price: "",
+        urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
+        location: {
+            latitude: 4.648825372481053,
+            length: -74.10133728464123
+        }
     }
 ];
 
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = 5001;
+const path = require("path");
 
 
 app.use(express.json());
+app.use(cors());
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -15,7 +18,9 @@ const eventsCalendar = require('./events');
 
 app.get('/event-list', (req, res) => {
     res.send(eventsCalendar)
-})
+});
+
+app.use('/photos', express.static(path.join(__dirname, 'images-events')));
 
 app.listen(port, () => {
     console.log(`Server run in http://locahost:${port}`);
