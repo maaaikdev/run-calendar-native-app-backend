4fe617744bedd17df6981613ce3df3e4654dc198
Michael Reyes <maaaik.dev@gmail.com>
2024-11-01 16:38:57 -0500

    feature/array distances and images events

--- events.js
+++ events.js
@@ -2,9 +2,11 @@ const eventsCalendar = [
     {
         id: 1,
         name: "Expedición - Bodytech - Bogotá",
-        date: "2024-09-22T8:00:00Z",
+        date: "2024-09-22T08:00:00Z",
         image: "images-events/2024/septiembre/bodytech/bodytech-kv.webp",
-        distance: "12k",
+        distance: [
+            { run: "12k" }
+        ],
         place: "Bogotá - Plaza de Alfiles",
         elevationGain: 0,
         category: "Running",
@@ -14,15 +16,17 @@ const eventsCalendar = [
         urlSite: "https://eventos.bodytech.co/event/info/expedicion-bodytech-bogota",
         location: {
             latitude: 4.648825372481053,
-            length: -74.10133728464123
+            longitude: -74.10133728464123
         }
     },
     {
         id: 2,
         name: "Expedición - Bodytech - Bogotá",
-        date: "2024-09-22T8:00:00Z",
+        date: "2024-09-22T08:00:00Z",
         image: "images-events/2024/septiembre/bodytech/bodytech-kv.webp",
-        distance: "6k",
+        distance: [
+            { run: "6k" }
+        ],
         place: "Bogotá - Plaza de Alfiles",
         elevationGain: 0,
         category: "Running",
@@ -32,15 +36,17 @@ const eventsCalendar = [
         urlSite: "https://eventos.bodytech.co/event/info/expedicion-bodytech-bogota",
         location: {
             latitude: 4.648825372481053,
-            length: -74.10133728464123
+            longitude: -74.10133728464123
         }
     },
     {
         id: 3,
         name: "Expedición - Bodytech - Bogotá",
-        date: "2024-09-22T8:00:00Z",
+        date: "2024-09-22T08:00:00Z",
         image: "images-events/2024/septiembre/bodytech/bodytech-kv.webp",
-        distance: "3k",
+        distance: [
+            { run: "3k" }
+        ],
         place: "Bogotá - Plaza de Alfiles",
         elevationGain: 0,
         category: "Running",
@@ -50,51 +56,57 @@ const eventsCalendar = [
         urlSite: "https://eventos.bodytech.co/event/info/expedicion-bodytech-bogota",
         location: {
             latitude: 4.648825372481053,
-            length: -74.10133728464123
+            longitude: -74.10133728464123
         }
     },
     {
         id: 4,
         name: "Corre con el Alma",
-        date: "2024-09-22T8:00:00Z",
-        image: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma-kv.jpeg",
-        distance: "5k",
+        date: "2024-09-22T08:00:00Z",
+        image: "images-events/2024/septiembre/corre-con-el-alma/corre-con-el-alma-kv.jpeg",
+        distance: [
+            { run: "5k" }
+        ],
         place: "Manizales",
         elevationGain: 0,
         category: "Running",
         organizationName: "Soul 23 sports",
-        organizationLogo: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma.jpg",
+        organizationLogo: "images-events/2024/septiembre/corre-con-el-alma/corre-con-el-alma.jpg",
         price: "",
         urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
         location: {
             latitude: 4.648825372481053,
-            length: -74.10133728464123
+            longitude: -74.10133728464123
         }
     },
     {
         id: 5,
         name: "Corre con el Alma",
-        date: "2024-09-22T8:00:00Z",
-        image: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma-kv.jpeg",
-        distance: "10k",
+        date: "2024-09-22T08:00:00Z",
+        image: "images-events/2024/septiembre/corre-con-el-alma/corre-con-el-alma-kv.jpeg",
+        distance: [
+            { run: "10k" }
+        ],
         place: "Manizales",
         elevationGain: 0,
         category: "Running",
         organizationName: "Soul 23 sports",
-        organizationLogo: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma.jpg",
+        organizationLogo: "images-events/2024/septiembre/corre-con-el-alma/corre-con-el-alma.jpg",
         price: "",
         urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
         location: {
             latitude: 4.648825372481053,
-            length: -74.10133728464123
+            longitude: -74.10133728464123
         }
     },
     {
         id: 6,
         name: "Corre con el Alma",
-        date: "2024-09-22T8:00:00Z",
-        image: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma-kv.jpeg",
-        distance: "15k",
+        date: "2024-09-22T08:00:00Z",
+        image: "images-events/2024/septiembre/corre-con-el-alma/corre-con-el-alma-kv.jpeg",
+        distance: [
+            { run: "15k" }
+        ],
         place: "Manizales",
         elevationGain: 0,
         category: "Running",
@@ -104,7 +116,29 @@ const eventsCalendar = [
         urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
         location: {
             latitude: 4.648825372481053,
-            length: -74.10133728464123
+            longitude: -74.10133728464123
+        }
+    },
+    {
+        id: 7,
+        name: "Duatlón",
+        date: "2024-11-03T08:00:00Z",
+        image: "images-events/2024/noviembre/duatlon/duatlon.png",
+        distance: [
+            { run: "3k" },
+            { bike: "20k" },
+            { run: "6k" }
+        ],
+        place: "Bogotá - Parque Simón Bolivar",
+        elevationGain: 0,
+        category: "Duatlón - Sprint",
+        organizationName: "Virtual Challenge",
+        organizationLogo: "images-events/2024/septiembre/corre-com-el-alma/corre-com-el-alma.png",
+        price: "",
+        urlSite: "https://biciq.com/info-event/%22corre-con-el-alma%22-e396638c",
+        location: {
+            latitude: 4.648825372481053,
+            longitude: -74.10133728464123
         }
     }
 ];
